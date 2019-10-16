3067d42c45ce6d765c3850581ab1b6cd11e5aabf
Zachary Whetstone <zw205715@ohio.edu>
2019-10-16 11:27:00 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200_Team12.Startup))]
+namespace MIS4200_Team12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
