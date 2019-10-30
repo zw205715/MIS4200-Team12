488bdbdecb17de7163d655aceacfbc80457135d4
Zachary Whetstone <zw205715@ohio.edu>
2019-10-30 11:10:21 -0400

    commit

---
+++ Controllers/TheProfilesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200_Team12.DAL;
+using MIS4200_Team12.Models;
+
+namespace MIS4200_Team12.Controllers
+{
+    public class TheProfilesController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: TheProfiles
+        public ActionResult Index()
+        {
+            return View(db.TheProfiles.ToList());
+        }
+
+        // GET: TheProfiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TheProfile theProfile = db.TheProfiles.Find(id);
+            if (theProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(theProfile);
+        }
+
+        // GET: TheProfiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TheProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "employeeID,firstName,lastName,email,phone,employeeSince")] TheProfile theProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TheProfiles.Add(theProfile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(theProfile);
+        }
+
+        // GET: TheProfiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TheProfile theProfile = db.TheProfiles.Find(id);
+            if (theProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(theProfile);
+        }
+
+        // POST: TheProfiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "employeeID,firstName,lastName,email,phone,employeeSince")] TheProfile theProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(theProfile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(theProfile);
+        }
+
+        // GET: TheProfiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TheProfile theProfile = db.TheProfiles.Find(id);
+            if (theProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(theProfile);
+        }
+
+        // POST: TheProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TheProfile theProfile = db.TheProfiles.Find(id);
+            db.TheProfiles.Remove(theProfile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIS4200_Team12.Models; // This is needed to access the models
+using System.Data.Entity; // this is needed to access the DbContext object
+
+namespace MIS4200_Team12.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public class Context : DbContext // inherits from DbContext
+        {
+            public Context() : base("name=DefaultConnection")
+            {
+                // this method is a 'constructor' and is called when a new context is created
+                // the base attribute says which connection string to use
+            }
+            // Include each object here. The value inside <> is the name of the class,
+            // the value outside should generally be the plural of the class name
+            // and is the name used to reference the entity in code
+            public DbSet<TheProfile> TheProfiles { get; set; }
+            public DbSet<Manager> Managers { get; set; }
+           
+        }
+
+        public System.Data.Entity.DbSet<MIS4200_Team12.Models.TheProfile> TheProfiles { get; set; }
+
+        public System.Data.Entity.DbSet<MIS4200_Team12.Models.Manager> Managers { get; set; }
+    }
+}
+
+
+
+
